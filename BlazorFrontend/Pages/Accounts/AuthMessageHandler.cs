5a7b0ba734cf2c9d4b9668ae4a41a6853c4d506c
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2025-12-07 18:07:43 +0100

    Add auth middleware to Blazor

---
+++ BlazorFrontend/Pages/Accounts/AuthMessageHandler.cs
@@ -0,0 +1,50 @@
+﻿namespace BlazorFrontend.Pages.Accounts;
+
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AuthMessageHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _localStorage;
+    private readonly AuthenticationStateProvider _authStateProvider;
+
+    public AuthMessageHandler(
+        ILocalStorageService localStorage,
+        AuthenticationStateProvider authStateProvider)
+    {
+        _localStorage = localStorage;
+        _authStateProvider = authStateProvider;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var token = await _localStorage.GetItemAsync<string>("authToken");
+
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        var response = await base.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+
+            if (_authStateProvider is CustomAuthStateProvider custom)
+            {
+                custom.NotifyUserLogout();
+            }
+        }
+
+        return response;
+    }
+}
+
--- BlazorFrontend/Program.cs
+++ BlazorFrontend/Program.cs
@@ -11,8 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp =>
-    new HttpClient { BaseAddress = new Uri("https://localhost:7204/") });
+//builder.Services.AddScoped(sp =>
+//    new HttpClient { BaseAddress = new Uri("https://localhost:7204/") });
 
 builder.Services.AddBlazoredLocalStorage();
 
@@ -20,6 +20,16 @@ builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped<CustomAuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<CustomAuthStateProvider>());
+
+builder.Services.AddTransient<AuthMessageHandler>();
+
+builder.Services.AddHttpClient("AuthorizedClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7204/");
+})
+.AddHttpMessageHandler<AuthMessageHandler>();
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("AuthorizedClient"));
 
 
 await builder.Build().RunAsync();
